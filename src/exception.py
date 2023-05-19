165985d58ec388ccbeff5a33ce98e92671fe0dbe
uditdas84 <uditdas84@gmail.com>
2023-05-19 22:03:20 +0530

    logging and exception modified

--- src/exception.py
+++ src/exception.py
@@ -24,9 +24,7 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Devided by zero")
+        logging.info("Devision by zero isn't possible")
         raise CustomException(e,sys)
 
-# a=1/0
-# print(a)
          
