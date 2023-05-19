c752724cc9ff0ecd671b42e331ca7cd18f00e5c1
uditdas84 <uditdas84@gmail.com>
2023-05-19 22:06:04 +0530

    logging and exception modified

--- src/exception.py
+++ src/exception.py
@@ -20,11 +20,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Devision by zero isn't possible")
-        raise CustomException(e,sys)
+
 
          
