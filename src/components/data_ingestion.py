cad954ac942d3971e4ad04545218f86a54d94a67
uditdas84 <uditdas84@gmail.com>
2023-06-27 17:51:13 +0530

    Hyper parameter tuning not needed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,14 +10,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainer
 from src.components.model_trainer import ModelTrainerConfig
-
-
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-
-    # os.makedirs(os.path.join(os.getcwd(),'artifacts'),exist_ok=True)
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -29,10 +25,9 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('C:\\mlproject\\notebook\\data\\stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            # os.makedirs(os.path.join(os.getcwd(),'artifacts'),exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -44,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -57,7 +52,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
